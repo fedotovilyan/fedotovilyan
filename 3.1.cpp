2a55ac2fb94912813ead09e87eeb7e81ba21157e
ilya <trusovdaniil03@mail.ru>
2019-10-20 16:35:35 +0300

    f

---
+++ 3.1.cpp
@@ -0,0 +1,18 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	const double dinf = 12;
+	double f, d;
+	cout << "Please enter your height:_";
+	cin >> d;
+	f = d / dinf;
+	cout << "Your height value is: " << f << " foots and " << d << " inch" << endl;
+	return 0;
+
+}
+
---
+++ 3.2.cpp
@@ -0,0 +1,27 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	const double f = 12;
+	const double d = 0.0254;
+	const double pb = 2.2;
+	double fh, mass, dh, imt;
+	cout << "Please enter your height in ft: ";
+	cin >> fh;
+	cout << "Please enter your mass: ";
+	cin >> mass;
+	cout << "Please enter your height in d: ";
+	cin >> dh;
+	mass = mass / pb;
+	fh = fh * f;
+	dh = (dh + fh) * d;
+	imt = mass / (dh * dh);
+	cout << imt << endl;
+	return 0;
+
+}
+
