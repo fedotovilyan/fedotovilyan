05ba733dff286be6047117b8044bbfdc83bd1893
ilya <trusovdaniil03@mail.ru>
2019-12-06 21:54:30 +0300

    16 variant

---
+++ lab_po_programmorovaniu_1_16v.cpp
@@ -0,0 +1,74 @@
+﻿#include <iostream>
+
+using namespace std;
+int main()
+{
+	float Xn, Xk, dX, a, b, c, x, y;
+	int j;
+	int d;
+	cout << "Введите диапазон и шаг изменения аргумента: ";
+	cin >> Xn;
+	cin >> Xk;
+	cin >> dX;
+	cout << "Введите значения a,b,c: ";
+	cin >> a;
+	cin >> b;
+	cin >> c;
+	j = 0;
+	d = 1;
+	int ac = a;
+	int bc = b;
+	int cc = c;
+	for (x = Xn; x <= Xk; x += dX) {
+		if ((x == 0) && (b != 0))
+			y = a * (x + c) * (x + c);
+		else {
+			if ((x == 0) && (b == 0)) {
+				y = (x - a) / -c;
+			}
+
+			else {
+				y = a + (x / c);
+			}
+
+		}
+		int yc = y;
+		if (d == 1) {
+			if ((((ac ^ bc) & (~(ac | cc)) != 0) && (yc != y)) || ((((ac ^ bc) & (~(ac | cc))) == 0) && (y == yc)))
+				d = 1;
+
+			else {
+				cout << "Введённые значения не удовлетворяют условию задачи" << endl;
+				d = 0;
+			}
+		}
+	}
+
+
+	for (x = Xn; x <= Xk; x += dX) {
+		if (d == 1) {
+			if ((x == 0) && (b != 0))
+				y = a * (x + c) * (x + c);
+			else {
+				if ((x == 0) && (b == 0)) {
+					y = (x - a) / -c;
+				}
+
+				else {
+					y = a + (x / c);
+				}
+				j += 1;
+
+				if ((j % 4 == 0) && (j >= 4)) {
+					cout << y << "  " << endl;
+				}
+				else {
+					cout << y << "  ";
+				}
+
+
+			}
+		}
+
+	}
+}
---
+++ lab_po_programmorovaniu_2_16v.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#include <conio.h>
+#include <math.h>
+
+using namespace std;
+int main()
+{
+	int n, koe, i, k, m;
+	double sum = 0;
+	koe = 0;
+	cout << "Укажите размер массива: ";
+	cin >> n;
+	double* arr = new double[n];
+	cout << "Введите значение каждого элемента массива: ";
+	for (i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+	double min = abs(arr[0]);
+	for (i = 0; i < n; i++) {
+		if (arr[i] < 0)
+			koe += 1;
+	}
+	for (i = 0; i < n; i++) {
+		if (abs(arr[i]) <= min) {
+			min = abs(arr[i]);
+			k = i;
+		}
+	}
+	m = k + 1;
+	for (m; m < n; m++) {
+		sum += abs(arr[m]);
+
+	}
+	for (i = 0; i < n; i++) {
+		if (arr[i] < 0)
+			arr[i] = arr[i] * arr[i];
+	}
+	int temp, j;
+	for (i = 0; i < n - 1; i++) {
+		for (j = 0; j < n - i - 1; j++) {
+			if (arr[j] > arr[j + 1]) {
+				temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
+		}
+	}
+	cout << "Количество отрицательных элементов массива: " << koe << endl;
+	cout << "Сумма модулей элементов массива: " << sum << endl;
+
+	for (i = 0; i < n; i++) {
+		cout << "Значение " << i << " элемента массива: " << arr[i] << endl;
+	}
+}
