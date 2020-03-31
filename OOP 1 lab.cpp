6b8c4883bb10ab33557b00c8c79333f4e94275d1
fedotovilyan <55277775+fedotovilyan@users.noreply.github.com>
2020-03-31 18:24:00 +0300

    Add files via upload

---
+++ OOP 1 lab.cpp
@@ -0,0 +1,33 @@
+﻿// ConsoleApplication3.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+class Point 
+{
+public:
+    int x;
+    int y;
+
+    vt()
+    {
+        cout << "(" << x << "," << y << ")" << endl;
+
+    }
+};
+
+
+
+
+
+
+int main()
+{
+    Point a;
+    a.x = 1;
+    a.y = 2;
+    a.vt();
+
+}
+
